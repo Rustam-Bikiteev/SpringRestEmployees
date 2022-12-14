330f2e225e6c1b63c9448d222923c738e355babf
Rustam Bikiteev <rust.bikiteev@gmail.com>
2022-12-14 15:03:59 +0300

    Add: GET 1 employee POST 1 employee PUT 1 Employee DELETE 1 employee

--- src/main/java/com/rustambikiteev/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/rustambikiteev/spring/rest/controller/MyRESTController.java
@@ -14,24 +14,43 @@ public class MyRESTController {
 
     @Autowired
     private EmployeeService employeeService;
-
     @GetMapping("/employees")
     public List<Employee> showAllEmployees(){
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
     }
-
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id){
         Employee employee = employeeService.getEmployee(id);
 
         if (employee == null){
-            throw new NoSuchEmployeeException("There is no ssuch employee with ID = " +
+            throw new NoSuchEmployeeException("There is no such employee with ID = " +
                     id + " in Database");
         }
+        return employee;
+    }
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
 
+        employeeService.saveEmployee(employee);
+        return employee;
+
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
         return employee;
     }
 
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable int id){
 
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no such employee with ID = " +
+                    id + "in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + "has been deleted";
+    }
 }
